05063ef630103311233a56486c958cbcd798b3f5
echo24404 <mika.urban24@gmail.com>
2025-03-28 14:25:53 +0100

    updated tasksModel functions (still some errors)

--- backend/models/tasksModel.js
+++ backend/models/tasksModel.js
@@ -1,54 +1,82 @@
 /**
  * @file tasksModel.js
- * @class TasksModel
- * Handles data loading and operations for user tasks.
+ * Provides methods to manage tasks in tasks.json.
  */
-export class TasksModel {
+const fs = require("fs");
+const path = require("path");
+
+class TasksModel {
     constructor() {
-        /** @type {Object} Will hold the tasks data after loading the JSON. */
-        this.tasksData = {};
+        // Pfad zur tasks.json
+        this.filePath = path.join(__dirname, "../data/tasks.json");
     }
 
     /**
-     * Loads tasks data from the tasks.json file using fetch.
-     * @returns {Promise<void>}
+     * Lädt alle Tasks aus der JSON-Datei.
+     * @returns {Object} tasksData
      */
-    async loadTasks() {
-        const response = await fetch('tasks.json');
-        this.tasksData = await response.json();
+    loadAllTasks() {
+        try {
+            const data = fs.readFileSync(this.filePath, "utf8");
+            return JSON.parse(data);
+        } catch (error) {
+            console.error("Error reading tasks.json:", error);
+            return {};
+        }
     }
 
     /**
-     * Returns an array of tasks for a specific user.
-     * @param {string} userId The ID of the user whose tasks are needed.
-     * @returns {Array} List of task objects.
+     * Schreibt das übergebene Object in die tasks.json.
+     * @param {Object} tasksData
+     */
+    saveAllTasks(tasksData) {
+        try {
+            fs.writeFileSync(
+                this.filePath,
+                JSON.stringify(tasksData, null, 2),
+                "utf8"
+            );
+        } catch (error) {
+            console.error("Error writing tasks.json:", error);
+        }
+    }
+
+    /**
+     * Gibt alle Tasks für einen bestimmten User zurück.
+     * @param {string} userId
+     * @returns {Array} Array der Task-Objekte
      */
     getTasksByUser(userId) {
-        return this.tasksData[userId] || [];
+        const allTasks = this.loadAllTasks();
+        return allTasks[userId] || [];
     }
 
     /**
-     * Adds a new task for the specified user.
-     * @param {string} userId The ID of the user to add the task to.
-     * @param {Object} task The task object to be added.
+     * Fügt einen neuen Task für einen bestimmten User hinzu.
+     * @param {string} userId
+     * @param {Object} task
      */
     addTask(userId, task) {
-        if (!this.tasksData[userId]) {
-            this.tasksData[userId] = [];
+        const allTasks = this.loadAllTasks();
+        if (!allTasks[userId]) {
+            allTasks[userId] = [];
         }
-        this.tasksData[userId].push(task);
-        // In a real app, you would send this change to an API or server as well
+        allTasks[userId].push(task);
+        this.saveAllTasks(allTasks);
     }
 
     /**
-     * Marks the given task index as done for a specific user.
-     * @param {string} userId The ID of the user.
-     * @param {number} taskIndex Index of the task in the array.
+     * Markiert eine Aufgabe als done.
+     * @param {string} userId
+     * @param {number} taskIndex Index der Aufgabe im Array
      */
     markTaskDone(userId, taskIndex) {
-        if (this.tasksData[userId] && this.tasksData[userId][taskIndex]) {
-            this.tasksData[userId][taskIndex].done = true;
-            // In a real app, persist this change on the server
+        const allTasks = this.loadAllTasks();
+        if (allTasks[userId] && allTasks[userId][taskIndex]) {
+            allTasks[userId][taskIndex].done = true;
+            this.saveAllTasks(allTasks);
         }
     }
 }
+
+module.exports = TasksModel;
