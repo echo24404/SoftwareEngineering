9248962ee26fafffc95814a2b9e04c051d902827
Fabse47 <fabio.behmueller@gmail.com>
2025-04-02 00:59:40 +0200

    changed route for ejs file

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../frontend/src/pages'));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('calender');
 });
 
 const PORT = 3000;
