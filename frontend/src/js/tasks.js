8a572a5cd80da221455def0ed597a9f7326cd589
echo24404 <mika.urban24@gmail.com>
2025-04-02 17:52:46 +0200

    small change

--- frontend/src/js/tasks.js
+++ frontend/src/js/tasks.js
@@ -8,7 +8,7 @@ import { TasksController } from '../../../backend/controllers/tasksController.js
 // This runs once the DOM is loaded
 document.addEventListener('DOMContentLoaded', async () => {
     // Example: Hardcoded user ID. In a real scenario, you might detect this from login info.
-    const userId = 'user1';
+    const userId = '1';
 
     const tasksController = new TasksController(userId);
     await tasksController.init();
