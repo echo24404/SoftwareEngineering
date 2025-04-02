80ce31f1960efb22bf7ba2225f49e1e3e7dffeca
Fabse47 <fabio.behmueller@gmail.com>
2025-04-02 23:39:53 +0200

    test impl for createCalendar

--- backend/tests/calendarModel.test.js
+++ backend/tests/calendarModel.test.js
@@ -7,3 +7,13 @@ const testFilePath = path.join(__dirname, '../data/calendar.json');
 beforeEach(() => {
     fs.writeFileSync(testFilePath, JSON.stringify([], null, 2));
 });
+
+describe('calendarModel', () => {
+    test('createCalendar erstellt neuen Kalender', () => {
+        const cal = calendarModel.createCalendar('u1', 'Testkalender', ['u2']);
+        expect(cal.name).toBe('Testkalender');
+
+        const alle = calendarModel.getCalendarsForUser('u1');
+        expect(alle.length).toBe(1);
+        expect(alle[0].name).toBe('Testkalender');
+    });
