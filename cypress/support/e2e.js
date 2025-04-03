e011c77768bac34e2265ab6aa23e8677f0229059
Fabse47 <fabio.behmueller@gmail.com>
2025-04-03 18:36:08 +0200

    cyprus test to create new calendar and add an event

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,3 +15,30 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+
+describe('Kalender-App E2E Flow', () => {
+    const calendarName = 'Testkalender E2E';
+    const eventTitle = 'WG-Putzplan';
+    const eventDate = '2025-04-01';
+
+    it('erstellt neuen Kalender und fügt einen Termin hinzu', () => {
+        cy.visit('http://localhost:3000/select-users');
+
+        cy.get('input[name="name"]').type(calendarName);
+
+        cy.get('input[type="checkbox"]').first().check();
+
+        cy.get('button[type="submit"]').click();
+
+        cy.contains(calendarName).should('exist');
+
+        cy.contains(calendarName).click();
+
+        // Termin hinzufügen
+        cy.get('input[type="date"]').type(eventDate);
+        cy.get('input[name="title"]').type(eventTitle);
+        cy.contains('+ Termin hinzufügen').click();
+
+        cy.contains(eventTitle).should('exist');
+    });
+});
