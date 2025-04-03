4bdc999716ff88a025c7abf0399093d9deb4c674
Tim <johler.tim@gmail.com>
2025-04-04 00:43:09 +0200

    Configruation of the test file

---
+++ backend/tests/shoppinglist.test.js
@@ -0,0 +1,29 @@
+// shoppinglist.test.js
+
+const request = require('supertest');
+const express = require('express');
+
+// Wir mocken das Model, sodass wir kontrollierte Testdaten erhalten.
+jest.mock('../models/shoppinglistModel', () => {
+    let data = {
+        "TestCategory": [
+            { name: "Item1", createdBy: "1", done: false }
+        ]
+    };
+    return {
+        getShoppingLists: jest.fn(() => Promise.resolve(data)),
+        updateShoppingLists: jest.fn((newData) => {
+            data = newData;
+            return Promise.resolve();
+        })
+    };
+});
+
+const shoppinglistRoutes = require('../routes/shoppinglistRoutes');
+
+// Erstelle eine Express-App für die Tests
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// Routen mounten – unter /shoppinglist
+app.use('/shoppinglist', shoppinglistRoutes);
